b028b6e19c99c650581ef2f34b56af01ca454576
Alex Gleason <alex@alexgleason.me>
2023-04-30 17:21:17 -0500

    Try disabling context endpoints

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,7 @@ app.get('/api/v1/accounts/lookup', accountLookupController);
 app.get('/api/v1/accounts/relationships', relationshipsController);
 app.get('/api/v1/accounts/:pubkey{[0-9a-f]{64}}', accountController);
 
-app.get('/api/v1/statuses/:id{[0-9a-f]{64}}/context', contextController);
+app.get('/api/v1/statuses/:id{[0-9a-f]{64}}/context', (c) => c.json({ ancestors: [], descendants: [] }));
 app.get('/api/v1/statuses/:id{[0-9a-f]{64}}', statusController);
 app.post('/api/v1/statuses', requireAuth, createStatusController);
 
